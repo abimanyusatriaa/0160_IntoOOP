54aa6f12d4291f02aee5cb075bfa49c041b98ea1
abimanyusatriaa <adji3890@gmail.com>
2024-04-23 11:36:11 +0700

    void input

--- 0160_IntoOOP/0160_IntoOOP.cpp
+++ 0160_IntoOOP/0160_IntoOOP.cpp
@@ -21,6 +21,14 @@ private:
     string namaMk;
     int sks;
 public:
+    void input() {
+        cout << "Kode MK: ";
+        cin >> kodeMk;
+        cout << "nama MK: ";
+        cin >> namaMk;
+        cout << "SKS: ";
+        cin >> sks;
+    }
 
 int main()
 {
