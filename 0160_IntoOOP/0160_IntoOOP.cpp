073738cebd1fcc8a88e146538c0466ef9fcfcdd4
abimanyusatriaa <adji3890@gmail.com>
2024-04-23 11:36:42 +0700

    void output

--- 0160_IntoOOP/0160_IntoOOP.cpp
+++ 0160_IntoOOP/0160_IntoOOP.cpp
@@ -30,6 +30,13 @@ public:
         cin >> sks;
     }
 
+    void output() {
+        cout << "Kode MK: " << kodeMk << endl;
+        cout << "Nama MK: " << namaMk << endl;
+        cout << "SKS: " << sks << endl;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
